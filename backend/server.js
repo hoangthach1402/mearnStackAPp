86c4d8ac05c094fe3abb310a5d06bbcbeab95860
hoangthach1402 <vuhoangthach1402@gmail.com>
2021-10-15 15:57:42 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import { notFound, errorHandler } from './middleware/errorMiddleWare.js'
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
 dotenv.config()
 
 connectDB()
@@ -11,15 +12,13 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('API is running  ...')
 })
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
 
+app.use('/api/products', productRoutes)
 const PORT = process.env.PORT || 5000
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(
   PORT,
