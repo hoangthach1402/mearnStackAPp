c24cc3eeab8ac3a346fad90c6654ac8df419af75
hoangthach1402 <vuhoangthach1402@gmail.com>
2021-10-15 10:32:23 +0700

    .

---
+++ backend/models/productModel.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    rating: { type: Number, require: true },
+    comment: { type: String, require: true },
+  },
+  {
+    timeStamps: true,
+  }
+)
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+    image: {
+      type: String,
+      require: true,
+    },
+    brand: {
+      type: String,
+      require: true,
+    },
+    category: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    review: [reviewSchema],
+    rating: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
+export default Product
